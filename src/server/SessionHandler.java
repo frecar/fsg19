b70a443e5f1d09db8f48cfcc4a1c001c07d4a3b4
Dag Erik Vikan <dvikan@gmail.com>
2011-03-30 18:22:55 +0200

    safd

--- src/server/SessionHandler.java
+++ src/server/SessionHandler.java
@@ -2,6 +2,7 @@ package server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.UUID;
 
 public class SessionHandler {
 
@@ -13,7 +14,17 @@ public class SessionHandler {
 		String requestLine = inFromClient.readLine();
 		System.out.println(ip + " says: " + requestLine);
 		
-		return requestLine.toUpperCase();
+		return processLogin(null, null); //return requestLine.toUpperCase();
+	}
+	
+	/**
+	 * Returns a session identifier if the username/password combination
+	 * exists
+	 */
+	public String processLogin(String username, String password) {
+		
+		String uid = UUID.randomUUID().toString().replaceAll("-", "");
+		return uid;
 	}
 
 
