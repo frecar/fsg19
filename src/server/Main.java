9edb74942833e4c89172efc5124b6b091babfd39
Dag Erik Vikan <dvikan@gmail.com>
2011-03-30 17:49:35 +0200

    liten endring i Main

--- src/server/Main.java
+++ src/server/Main.java
@@ -12,16 +12,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		DatabaseHandler db = DatabaseHandler.getInstance();
-		db.lol();
-		
 		if(args.length != 1) {
 			System.out.println("Usage: java -jar Main.jar <port> \n" +
 			"For example: java -jar Main.jar 80");
 			System.exit(0);
 		}
 		else {
-			//new Main(args[0]);
+			new Main(args[0]);
 		}
 	}
 
